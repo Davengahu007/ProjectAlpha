1b0e05d089e08dcd735e17c11637739e11a02121
Davengahu007 <davengahu007@gmail.com>
2024-04-23 21:06:27 +0300

    Initial commit

--- src/main/java/com/dsystems/ProjectAlpha/Controller/ServerController.java
+++ src/main/java/com/dsystems/ProjectAlpha/Controller/ServerController.java
@@ -1,4 +1,6 @@
 package com.dsystems.ProjectAlpha.Controller;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.HashMap;
@@ -7,17 +9,20 @@ import java.util.Map;
 @RestController
 public class ServerController {
 
+    @Value("${server.id}")
+    private String serverId;
+
     @GetMapping("/home")
-    public Map<String, String> home() {
+    public ResponseEntity<Map<String, String>> home() {
         Map<String, String> response = new HashMap<>();
-        response.put("message", "Hello from Server: 1");
-        return response;
+        response.put("message", "Hello from Server: " + serverId);
+        response.put("status", "successful");
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/heartbeat")
-    public Map<String, String> heartbeat() {
-        Map<String, String> response = new HashMap<>();
-        response.put("status", "OK");
-        return response;
+    public ResponseEntity<Void> heartbeat() {
+        return ResponseEntity.ok().build();
     }
 }
+
